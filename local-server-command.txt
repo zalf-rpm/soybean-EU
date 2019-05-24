b3b341ee356b6b430f700faa4a4ce2c3e3777a7c
TStella <tommaso.stella@zalf.de>
2019-05-24 16:56:47 +0200

    first version of producer

---
+++ local-server-command.txt
@@ -0,0 +1,3 @@
+cd C:\Users\stella\Documents\GitHub\monica
+
+_cmake_win64\Release\monica-zmq-server -bi -i tcp://*:6666 -bo -o tcp://*:7777
---
+++ run-work-producer.py
@@ -0,0 +1,331 @@
+#!/usr/bin/python
+# -*- coding: UTF-8
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+# Authors:
+# Michael Berg-Mohnicke <michael.berg@zalf.de>
+# Tommaso Stella <tommaso.stella@zalf.de> 
+#
+# Maintainers:
+# Currently maintained by the authors.
+#
+# This file has been created at the Institute of
+# Landscape Systems Analysis at the ZALF.
+# Copyright (C: Leibniz Centre for Agricultural Landscape Research (ZALF)
+
+import time
+import os
+import json
+import csv
+from copy import deepcopy
+from datetime import date, timedelta
+#import types
+import sys
+#sys.path.insert(0, "C:\\Users\\berg.ZALF-AD\\GitHub\\monica\\project-files\\Win32\\Release")
+#sys.path.insert(0, "C:\\Users\\berg.ZALF-AD\\GitHub\\monica\\project-files\\Win32\\Debug")
+#sys.path.insert(0, "C:\\Users\\berg.ZALF-AD\\GitHub\\monica\\src\\python")
+#sys.path.insert(0, "C:\\Program Files (x86)\\MONICA")
+#print sys.path
+#sys.path.append('C:/Users/berg.ZALF-AD/GitHub/util/soil')
+#from soil_conversion import *
+#import monica_python
+import zmq
+import monica_io
+#print "path to monica_io: ", monica_io.__file__
+
+#print "pyzmq version: ", zmq.pyzmq_version()
+#print "sys.path: ", sys.path
+#print "sys.version: ", sys.version
+
+USER_MODE = "localProducer-localMonica"
+#USER_MODE = "localProducer-remoteMonica"
+
+PATHS = {
+    # adjust the local path to your environment
+    "localProducer-localMonica": {
+        "monica-parameters-path": "C:/Users/stella/Documents/GitHub/monica-parameters/", # path to monica-parameters
+        "monica-path-to-climate-dir": "A:/projects/macsur-eu-heat-stress-assessment/climate-data/transformed/", # mounted path to archive accessable by monica executable
+    },
+    "localProducer-remoteMonica": {
+        "monica-parameters-path": "C:/Users/stella/Documents/GitHub/monica-parameters/", # path to monica-parameters
+        "monica-path-to-climate-dir": "/archiv-daten/md/projects/macsur-eu-heat-stress-assessment/climate-data/transformed/", # mounted path to archive accessable by monica executable
+    }
+}
+
+CONFIGURATION = {
+        "mode": USER_MODE,
+        "server": "localhost",
+        "server-port": "6666",
+        "start-row": 1, 
+        "end-row": 8157,
+        "run-periods": ["0", "2", "3"]
+    }
+
+script_path = os.path.dirname(os.path.abspath(__file__))
+
+#PATH_TO_CLIMATE_DATA_SERVER = "/archiv-daten/md/projects/macsur-eu-heat-stress-assessment/climate-data/transformed/"
+#INCLUDE_FILE_BASE_PATH = "C:/Users/berg.ZALF-AD.000/MONICA"
+
+def run_producer(config):
+    "main"
+
+    print "config:", config
+
+    context = zmq.Context()
+    socket = context.socket(zmq.PUSH)
+    
+    # select paths 
+    paths = PATHS[config["mode"]]
+    
+    # connect to monica proxy (if local, it will try to connect to a locally started monica)
+    socket.connect("tcp://" + config["server"] + ":" + str(config["server-port"]))
+    
+    template_folder = script_path + "/json_templates/"
+    with open(template_folder + "sim.json") as _:
+        sim = json.load(_)
+        sim["include-file-base-path"] = paths["monica-parameters-path"]
+
+    with open(template_folder + "site.json") as _:
+        site = json.load(_)
+
+    with open(template_folder +"crop.json") as _:
+        crop = json.load(_)
+
+    with open(template_folder + "sims.json") as _:
+        sims = json.load(_)
+
+    period_gcm_co2s = [
+        {"id": "C1", "period": "0", "gcm": "0_0", "co2_value": 360},
+        {"id": "C2", "period": "2", "gcm": "GFDL-CM3_45", "co2_value": 360},
+        {"id": "C3", "period": "3", "gcm": "GFDL-CM3_45", "co2_value": 360},
+        {"id": "C4", "period": "2", "gcm": "GFDL-CM3_85", "co2_value": 360},
+        {"id": "C5", "period": "3", "gcm": "GFDL-CM3_85", "co2_value": 360},
+        {"id": "C6", "period": "2", "gcm": "GISS-E2-R_45", "co2_value": 360},
+        {"id": "C7", "period": "3", "gcm": "GISS-E2-R_45", "co2_value": 360},
+        {"id": "C8", "period": "2", "gcm": "GISS-E2-R_85", "co2_value": 360},
+        {"id": "C9", "period": "3", "gcm": "GISS-E2-R_85", "co2_value": 360},
+        {"id": "C10", "period": "2", "gcm": "HadGEM2-ES_26", "co2_value": 360},
+        {"id": "C11", "period": "3", "gcm": "HadGEM2-ES_26", "co2_value": 360},
+        {"id": "C12", "period": "2", "gcm": "HadGEM2-ES_45", "co2_value": 360},
+        {"id": "C13", "period": "3", "gcm": "HadGEM2-ES_45", "co2_value": 360},
+        {"id": "C14", "period": "2", "gcm": "HadGEM2-ES_85", "co2_value": 360},
+        {"id": "C15", "period": "3", "gcm": "HadGEM2-ES_85", "co2_value": 360},
+        {"id": "C16", "period": "2", "gcm": "MIROC5_45", "co2_value": 360},
+        {"id": "C17", "period": "3", "gcm": "MIROC5_45", "co2_value": 360},
+        {"id": "C18", "period": "2", "gcm": "MIROC5_85", "co2_value": 360},
+        {"id": "C19", "period": "3", "gcm": "MIROC5_85", "co2_value": 360},
+        {"id": "C20", "period": "2", "gcm": "MPI-ESM-MR_26", "co2_value": 360},
+        {"id": "C21", "period": "3", "gcm": "MPI-ESM-MR_26", "co2_value": 360},
+        {"id": "C22", "period": "2", "gcm": "MPI-ESM-MR_45", "co2_value": 360},
+        {"id": "C23", "period": "3", "gcm": "MPI-ESM-MR_45", "co2_value": 360},
+        {"id": "C24", "period": "2", "gcm": "MPI-ESM-MR_85", "co2_value": 360},
+        {"id": "C25", "period": "3", "gcm": "MPI-ESM-MR_85", "co2_value": 360},
+        {"id": "C26", "period": "2", "gcm": "GFDL-CM3_45", "co2_value": 499},
+        {"id": "C27", "period": "3", "gcm": "GFDL-CM3_45", "co2_value": 532},
+        {"id": "C28", "period": "2", "gcm": "GFDL-CM3_85", "co2_value": 571},
+        {"id": "C29", "period": "3", "gcm": "GFDL-CM3_85", "co2_value": 801},
+        {"id": "C30", "period": "2", "gcm": "GISS-E2-R_45", "co2_value": 499},
+        {"id": "C31", "period": "3", "gcm": "GISS-E2-R_45", "co2_value": 532},
+        {"id": "C32", "period": "2", "gcm": "GISS-E2-R_85", "co2_value": 571},
+        {"id": "C33", "period": "3", "gcm": "GISS-E2-R_85", "co2_value": 801},
+        {"id": "C34", "period": "2", "gcm": "HadGEM2-ES_26", "co2_value": 442},
+        {"id": "C35", "period": "3", "gcm": "HadGEM2-ES_26", "co2_value": 429},
+        {"id": "C36", "period": "2", "gcm": "HadGEM2-ES_45", "co2_value": 499},
+        {"id": "C37", "period": "3", "gcm": "HadGEM2-ES_45", "co2_value": 532},
+        {"id": "C38", "period": "2", "gcm": "HadGEM2-ES_85", "co2_value": 571},
+        {"id": "C39", "period": "3", "gcm": "HadGEM2-ES_85", "co2_value": 801},
+        {"id": "C40", "period": "2", "gcm": "MIROC5_45", "co2_value": 499},
+        {"id": "C41", "period": "3", "gcm": "MIROC5_45", "co2_value": 532},
+        {"id": "C42", "period": "2", "gcm": "MIROC5_85", "co2_value": 571},
+        {"id": "C43", "period": "3", "gcm": "MIROC5_85", "co2_value": 801},
+        {"id": "C44", "period": "2", "gcm": "MPI-ESM-MR_26", "co2_value": 442},
+        {"id": "C45", "period": "3", "gcm": "MPI-ESM-MR_26", "co2_value": 429},
+        {"id": "C46", "period": "2", "gcm": "MPI-ESM-MR_45", "co2_value": 499},
+        {"id": "C47", "period": "3", "gcm": "MPI-ESM-MR_45", "co2_value": 532},
+        {"id": "C48", "period": "2", "gcm": "MPI-ESM-MR_85", "co2_value": 571},
+        {"id": "C49", "period": "3", "gcm": "MPI-ESM-MR_85", "co2_value": 801}
+    ]
+
+    soil = {}
+    row_cols = []
+    with open("JRC_soil_macsur_v3.csv") as _:
+        reader = csv.reader(_)
+        reader.next()
+        for row in reader:
+            row_col = (int(row[1]), int(row[0]))
+            row_cols.append(row_col)
+            soil[row_col] = {
+                "elevation": float(row[4]),
+                "latitude": float(row[5]),
+                "depth": float(row[6]),
+                "pwp": float(row[7]),
+                "fc": float(row[8]),
+                "sat": float(row[9]),
+                "sw-init": float(row[10]),
+                "oc-topsoil": float(row[11]),
+                "oc-subsoil": float(row[12]),
+                "bd-topsoil": float(row[13]),
+                "bd-subsoil": float(row[14]),
+                "sand-topsoil": float(row[15]),
+                "sand-subsoil": float(row[18]),
+                "clay-topsoil": float(row[16]),
+                "clay-subsoil": float(row[19]),
+            }
+
+    def read_sowing_doys(path_to_file):
+        with open(path_to_file) as _:
+            sowing_doys = {}
+            reader = csv.reader(_)
+            reader.next()
+            for line in reader:
+                cell_doys = []
+                row_, col_ = line[0].split("_")
+                row, col = (int(row_), int(col_))
+                for i in range(1, len(line)):
+                    cell_doys.append(int(line[i]))
+                sowing_doys[(row, col)] = cell_doys
+            return sowing_doys
+
+    sowing_doys = read_sowing_doys("sowing_dates_soybean.csv")
+
+    def get_custom_site(row, col):
+        "update function"
+        cell_soil = soil[(row, col)]
+        
+        pwp = cell_soil["pwp"]
+        fc_ = cell_soil["fc"]
+        sm_percent_fc = cell_soil["sw-init"] / fc_ * 100.0
+        
+        top = {
+            "Thickness": [0.3, "m"],
+            "SoilOrganicCarbon": [cell_soil["oc-topsoil"], "%"],
+            "SoilBulkDensity": [cell_soil["bd-topsoil"] * 1000, "kg m-3"],
+            "FieldCapacity": [fc_, "m3 m-3"],
+            "PermanentWiltingPoint": [pwp, "m3 m-3"],
+            "PoreVolume": [cell_soil["sat"], "m3 m-3"],
+            "SoilMoisturePercentFC": [sm_percent_fc, "% [0-100]"],
+            "Sand": cell_soil["sand-topsoil"] / 100.0,
+            "Clay": cell_soil["clay-topsoil"] / 100.0
+            }
+        sub = {
+            "Thickness": [1.7, "m"],
+            "SoilOrganicCarbon": [cell_soil["oc-subsoil"], "%"],
+            "SoilBulkDensity": [cell_soil["bd-subsoil"] * 1000, "kg m-3"],
+            "FieldCapacity": [fc_, "m3 m-3"],
+            "PermanentWiltingPoint": [pwp, "m3 m-3"],
+            "PoreVolume": [cell_soil["sat"], "m3 m-3"],
+            "SoilMoisturePercentFC": [sm_percent_fc, "% [0-100]"],
+            "Sand": cell_soil["sand-subsoil"] / 100.0,
+            "Clay": cell_soil["clay-subsoil"] / 100.0
+        }
+
+        custom_site = {
+            "soil-profile": [top, sub],
+            "latitude": cell_soil["latitude"],
+            "sw-init": cell_soil["sw-init"],
+        }
+        
+        return custom_site
+
+
+    #assert len(row_cols) == len(pheno["GM"].keys()) == len(pheno["WW"].keys())
+    print "# of rowsCols = ", len(row_cols)
+
+    i = 0
+    start_store = time.clock()
+    start = config["start-row"] - 1
+    end = config["end-row"] - 1
+    row_cols_ = row_cols[start:end+1]
+    #row_cols_ = [(108,106), (89,82), (71,89), (58,57), (77,109), (66,117), (46,151), (101,139), (116,78), (144,123)]
+    #row_cols_ = [(66,117)]
+    print "running from ", start, "/", row_cols[start], " to ", end, "/", row_cols[end]
+    
+    for row, col in row_cols_:
+                
+        custom_site = get_custom_site(row, col)
+
+        site["Latitude"] = custom_site["latitude"]
+        site["SiteParameters"]["SoilProfileParameters"] = custom_site["soil-profile"]
+
+        for crop_id in crop["crops"].keys():
+            for s_doy in sowing_doys[(row, col)]:
+                s_date = date(2019, 1, 1) + timedelta(days=s_doy-1)
+                for ws in crop["cropRotation"][0]["worksteps"]:
+                    if ws["type"] == "Sowing":
+                        #set sowing dates        
+                        ws["date"] = unicode(s_date.strftime("0000-%m-%d"))
+                        #set crop ref
+                        ws["crop"][2] = crop_id
+                    if ws["type"] == "SetValue":
+                        #set mois
+                        ws["value"] = custom_site["sw-init"]
+                
+                env = monica_io.create_env_json_from_json_config({
+                    "crop": crop,
+                    "site": site,
+                    "sim": sim,
+                    "climate": ""
+                })
+                
+                env["csvViaHeaderOptions"] = sim["climate.csv-options"]
+
+                for pgc in period_gcm_co2s:
+                    co2_id = pgc["id"]
+                    co2_value = pgc["co2_value"]
+                    period = pgc["period"]
+                    gcm = pgc["gcm"]
+
+                    if period not in config["run-periods"]:
+                        continue
+
+                    env["params"]["userEnvironmentParameters"]["AtmosphericCO2"] = co2_value
+
+                    climate_filename = "{}_{:03d}_v1.csv".format(row, col)
+                    #if not os.path.exists(path_to_climate_file):
+                    #    continue
+
+                    #read climate data on the server and send just the path to the climate data csv file
+                    env["pathToClimateCSV"] = paths["monica-path-to-climate-dir"] + period + "/" + gcm + "/" + climate_filename
+
+                    env["events"] = sims["output"]
+
+                    for sim_ in sims["treatments"]:
+                        env["params"]["simulationParameters"]["UseAutomaticIrrigation"] = sim_["UseAutomaticIrrigation"]
+                        env["params"]["simulationParameters"]["AutoIrrigationParams"]["amount"] = sims["irrigation-amount"][crop_id]
+                                            
+                        env["customId"] = str(row) + "/" + str(col) + ")" \
+                                            + "|" + period \
+                                            + "|" + gcm \
+                                            + "|(" + co2_id + "/" + str(co2_value) + ")" \
+                                            + "|" + sim_["TrtNo"] \
+                                            + "|" + sim_["ProdCase"]
+
+                        socket.send_json(env)
+                        print "sent env ", i, " customId: ", env["customId"]
+                        i += 1
+                        #break
+                    #break
+                #break
+
+    stop_store = time.clock()
+
+    print "sending ", i, " envs took ", (stop_store - start_store), " seconds"
+    print "ran from ", start, "/", row_cols[start], " to ", end, "/", row_cols[end]
+    return
+
+
+if __name__ == "__main__":
+
+    config = deepcopy(CONFIGURATION)
+
+    # read commandline args only if script is invoked directly from commandline
+    if len(sys.argv) > 1 and __name__ == "__main__":
+        for arg in sys.argv[1:]:
+            k, v = arg.split("=")
+            if k in config:
+                config[k] = v
+
+    run_producer(config)
