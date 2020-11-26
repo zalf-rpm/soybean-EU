f189fa348315c5ed02da71356002c83d0fd842d7
sanny <38938782+sannyschulz@users.noreply.github.com>
2020-11-26 20:37:41 +0100

    wip

--- create_image_from_ascii.py
+++ create_image_from_ascii.py
@@ -11,6 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 from matplotlib.backends.backend_pdf import PdfPages
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 from datetime import datetime
 import collections
 import errno
@@ -130,10 +131,13 @@ def build() :
 @dataclass
 class Image:
     name: str
+    title: str
     content: list
 
 @dataclass
 class Row:
+    subtitle: str
+    sharedColorBar: bool
     content: list
 
 @dataclass
@@ -159,14 +163,26 @@ def readSetup(filename, root, files) :
         metapath = os.path.join(root, metafilename)
         return File(filepath, metapath)   
 
-    def readRow(doc) : 
+    def readRows(doc) : 
         rowContent = list()
-        for entry, entrydoc in doc.items() :
-            if entry == "file" :
-                rowContent.append(readFile(entrydoc))
-            if entry == "merge" :
-                rowContent.append(readMerge(entrydoc))
-        return Row(rowContent)
+        for rows in doc :
+            for row in rows:
+                if row == "row" :
+                    rowList = list()
+                    sharedColorBar = False
+                    subtitle = ""
+                    for entries in rows["row"]:
+                        for entry in entries :
+                            if entry == "sharedColorBar" :
+                                sharedColorBar = bool(entries["sharedColorBar"])
+                            if entry == "subtitle" :
+                                subtitle = entries["subtitle"]
+                            if entry == "file" :
+                                rowList.append(readFile(entries["file"]))
+                            if entry == "merge" :
+                                rowList.append(readMerge(entries["merge"]))
+                    rowContent.append(Row(subtitle, sharedColorBar, rowList))
+        return rowContent
 
     def readMerge(doc) : 
         mergeContent = list()
@@ -180,22 +196,25 @@ def readSetup(filename, root, files) :
         yaml=YAML(typ='safe')   # default, if not specfied, is 'rt' (round-trip)
         documents = yaml.load(source)
         #documents = yaml.full_load(meta)
-        for item, doc in documents.items():
-            print(item, ":", doc)
-            if item == "image" :
+        for item in documents:
+            print(item)
+            if "image" in item:
                 indexImg = indexImg + 1 
                 imagename = "none" + str(indexImg)
+                title = ""
                 imageContent = list()
-                for entry, entrydoc in doc.items() :
-                    if entry == "name" :
-                        imagename = entrydoc
+                for entry in item["image"] :
+                    if entry == "name"  :
+                        imagename = item["image"][entry]
+                    if entry == "title"  :
+                        title = item["image"][entry]
                     elif entry == "file" :
-                        imageContent.append(File(entrydoc))
-                    elif entry == "row" :
-                        imageContent.append(readRow(doc))
+                        imageContent.append(File(item["image"][entry]))
+                    elif entry == "rows" :
+                        imageContent = readRows(item["image"][entry])
                     elif entry == "merge" :
-                        imageContent.append(readMerge(doc))
-                imageList.append(Image(imagename, imageContent))
+                        imageContent.append(readMerge(item["image"][entry]))
+                imageList.append(Image(imagename, title, imageContent))
     return imageList
 
 
@@ -392,8 +411,9 @@ class Meta:
     maxLoaded: bool
     minValue: float
     minLoaded: bool
+    showbars: bool
 
-def readMeta(meta_path, ascii_nodata) :
+def readMeta(meta_path, ascii_nodata, showCBar) :
     title="" 
     label=""
     colormap = 'viridis'
@@ -406,6 +426,7 @@ def readMeta(meta_path, ascii_nodata) :
     maxLoaded = False
     minValue = ascii_nodata
     minLoaded = False
+    showbars = showCBar
 
     with open(meta_path, 'rt') as meta:
        # documents = yaml.load(meta, Loader=yaml.FullLoader)
@@ -442,7 +463,7 @@ def readMeta(meta_path, ascii_nodata) :
     maxValue *= factor
     minValue *= factor
     return Meta(title, label, colormap, minColor, cMap,
-                cbarLabel, factor, ticklist, maxValue, maxLoaded, minValue, minLoaded)
+                cbarLabel, factor, ticklist, maxValue, maxLoaded, minValue, minLoaded, showbars)
 
 
 def createSubPlot(image, out_path, pdf=None) :
@@ -451,41 +472,50 @@ def createSubPlot(image, out_path, pdf=None) :
     nplotCols = 0
     asciiHeaderLs = dict()
     metaLs = dict()
+    subtitles = list()
     for content in image.content :
 
-        if content is File :
+        if type(content) is File :
             name = content.name
             meta = content.meta
             asciiHeader = readAsciiHeader(name)
-            meta = readMeta(meta, asciiHeader.ascii_nodata)  
+            meta = readMeta(meta, asciiHeader.ascii_nodata, True)  
             nplotRows += 1
             nplotCols += 1             
             asciiHeaderLs[(nplotRows, nplotCols)] = asciiHeader 
             metaLs[(nplotRows, nplotCols)] = meta
             break
-        elif content is Row :
+        elif type(content) is Row :
             nplotRows += 1
+            numCol = 0
+            subtitles.append(content.subtitle)
+            shareCBar = content.sharedColorBar
+            lastCol = len(content.content)
             for col in content.content :
-                nplotCols += 1
-                if col is File :
-                    asciiHeader = readAsciiHeader(content.name)         
-                    asciiHeaderLs[(nplotRows, nplotCols)] = asciiHeader
-                    metaLs[(nplotRows, nplotCols)] = readMeta(content.meta, asciiHeader.ascii_nodata) 
-                elif col is Merge :
+                numCol += 1
+                showBar = not shareCBar or (numCol == lastCol) 
+                if type(col) is File :
+                    asciiHeader = readAsciiHeader(col.name)         
+                    asciiHeaderLs[(nplotRows, numCol)] = asciiHeader
+                    metaLs[(nplotRows, numCol)] = readMeta(col.meta, asciiHeader.ascii_nodata, showBar) 
+                elif type(col) is Merge :
                     mergeHeaderList = list()
                     mergeMetaList = list()
                     for f in col.content :
                         asciiHeader = readAsciiHeader(f.name)         
-                        meta = readMeta(f.meta, asciiHeader.ascii_nodata) 
+                        meta = readMeta(f.meta, asciiHeader.ascii_nodata, showBar) 
                         mergeHeaderList.append(asciiHeader)
                         mergeMetaList.append(meta)
-                    asciiHeaderLs[(nplotRows, nplotCols)] = mergeHeaderList
-                    metaLs[(nplotRows, nplotCols)] = mergeMetaList
+                    asciiHeaderLs[(nplotRows, numCol)] = mergeHeaderList
+                    metaLs[(nplotRows, numCol)] = mergeMetaList
 
-        elif content is Merge:
+            if numCol > nplotCols : 
+                nplotCols = numCol
+                
+        elif type(content) is Merge:
             for f in col.content :
                 asciiHeader = readAsciiHeader(f.name)         
-                meta = readMeta(f.meta, asciiHeader.ascii_nodata) 
+                meta = readMeta(f.meta, asciiHeader.ascii_nodata, True) 
                 mergeHeaderList.append(asciiHeader)
                 mergeMetaList.append(meta)
             nplotRows += 1
@@ -493,84 +523,97 @@ def createSubPlot(image, out_path, pdf=None) :
             asciiHeaderLs[(nplotRows, nplotCols)] = mergeHeaderList
             metaLs[(nplotRows, nplotCols)] = mergeMetaList
             break
-        # Plot data array
-        # fig, ax = plt.subplots()
-        # ax.set_title(title)
-        
-        fig, axs = plt.subplots(nrows=nplotRows, ncols=nplotCols, squeeze=False)
-        #fig.subplots_adjust(top=0.95, bottom=0.01, left=0.2, right=0.99,
-        #                   wspace=0.05)
-
-        #fig.suptitle('historical     future', fontsize=14, y=1.0, x=0.6)
-
-        for idxRow in range(1,nplotRows+1) :
-            for idxCol in range(1,nplotCols+1) :
-                ax = axs[idxRow-1][idxCol-1]
-                asciiHeader = asciiHeaderLs[(idxRow,idxCol)]
-                meta = metaLs[(idxRow,idxCol)]
-                if asciiHeader is not list :
-                    # Read in the ascii data array
-                    ascii_data_array = np.loadtxt(asciiHeader.ascii_paths, dtype=np.float, skiprows=6)
-            
-                    # Set the nodata values to nan
-                    ascii_data_array[ascii_data_array == asciiHeader.ascii_nodata] = np.nan
-
-                    # data is stored as an integer but scaled by a factor
-                    ascii_data_array *= meta.factor
+    # Plot data array
+    # fig, ax = plt.subplots()
+    # ax.set_title(title)
     
-                    # set min color if given
-                    if len(meta.minColor) > 0 and not meta.cMap:
-                        newColorMap = matplotlib.cm.get_cmap(meta.colormap, 256)
-                        newcolors = newColorMap(np.linspace(0, 1, 256))
-                        rgba = matplotlib.cm.colors.to_rgba(meta.minColor)
-                        minColorVal = np.array([rgba])
-                        newcolors[:1, :] = minColorVal
-                        colorM = ListedColormap(newcolors)
-                        if meta.minLoaded and meta.maxLoaded:
-                            img_plot = ax.imshow(ascii_data_array, cmap=colorM, extent=asciiHeader.image_extent, interpolation='none', vmin=meta.minValue, vmax=meta.maxValue)
-                        elif meta.minLoaded :
-                            img_plot = ax.imshow(ascii_data_array, cmap=colorM, extent=asciiHeader.image_extent, interpolation='none', vmax=meta.minValue)
-                        elif meta.maxLoaded :
-                            img_plot = ax.imshow(ascii_data_array, cmap=colorM, extent=asciiHeader.image_extent, interpolation='none', vmax=meta.maxValue)
-                        else :
-                            img_plot = ax.imshow(ascii_data_array, cmap=colorM, extent=asciiHeader.image_extent, interpolation='none')
+    fig, axs = plt.subplots(nrows=nplotRows, ncols=nplotCols, squeeze=False, sharex=True, sharey=True, figsize=(8,14))
+    fig.subplots_adjust(top=0.95, bottom=0.01, left=0.2, right=0.99, wspace=0.05)
+    
+    if image.title :
+        fig.suptitle(image.title, fontsize='xx-large')
 
-                    # Get the img object in order to pass it to the colorbar function
-                    elif meta.cMap :
-                        colorM = ListedColormap(meta.cMap)
-                        if meta.minLoaded and meta.maxLoaded:
-                            img_plot = ax.imshow(ascii_data_array, cmap=colorM, extent=asciiHeader.image_extent, interpolation='none', vmin=meta.minValue, vmax=meta.maxValue)
-                        elif meta.minLoaded :
-                            img_plot = ax.imshow(ascii_data_array, cmap=colorM, extent=asciiHeader.image_extent, interpolation='none', vmax=meta.minValue)
-                        elif meta.maxLoaded :
-                            img_plot = ax.imshow(ascii_data_array, cmap=colorM, extent=asciiHeader.image_extent, interpolation='none', vmax=meta.maxValue)
-                        else :
-                            img_plot = ax.imshow(ascii_data_array, cmap=colorM, extent=asciiHeader.image_extent, interpolation='none')
+    #fig.suptitle('historical     future', fontsize=14, y=1.0, x=0.6)
+
+    for idxRow in range(1,nplotRows+1) :
+        # if len(subtitles) >= idxRow and len(subtitles[idxRow-1]) > 0:
+        #     fig.suptitle(subtitles[idxRow-1])
+        for idxCol in range(1,nplotCols+1) :
+            ax = axs[idxRow-1][idxCol-1]
+            asciiHeader = asciiHeaderLs[(idxRow,idxCol)]
+            meta = metaLs[(idxRow,idxCol)]
+            if type(asciiHeader) is not list :
+                # Read in the ascii data array
+                ascii_data_array = np.loadtxt(asciiHeader.ascii_path, dtype=np.float, skiprows=6)
+        
+                # Set the nodata values to nan
+                ascii_data_array[ascii_data_array == asciiHeader.ascii_nodata] = np.nan
+
+                # data is stored as an integer but scaled by a factor
+                ascii_data_array *= meta.factor
+
+                # set min color if given
+                if len(meta.minColor) > 0 and not meta.cMap:
+                    newColorMap = matplotlib.cm.get_cmap(meta.colormap, 256)
+                    newcolors = newColorMap(np.linspace(0, 1, 256))
+                    rgba = matplotlib.cm.colors.to_rgba(meta.minColor)
+                    minColorVal = np.array([rgba])
+                    newcolors[:1, :] = minColorVal
+                    colorM = ListedColormap(newcolors)
+                    if meta.minLoaded and meta.maxLoaded:
+                        img_plot = ax.imshow(ascii_data_array, cmap=colorM, extent=asciiHeader.image_extent, interpolation='none', vmin=meta.minValue, vmax=meta.maxValue)
+                    elif meta.minLoaded :
+                        img_plot = ax.imshow(ascii_data_array, cmap=colorM, extent=asciiHeader.image_extent, interpolation='none', vmax=meta.minValue)
+                    elif meta.maxLoaded :
+                        img_plot = ax.imshow(ascii_data_array, cmap=colorM, extent=asciiHeader.image_extent, interpolation='none', vmax=meta.maxValue)
                     else :
-                        if meta.minLoaded and meta.maxLoaded:
-                            img_plot = ax.imshow(ascii_data_array, cmap=meta.colormap, extent=asciiHeader.image_extent, interpolation='none', vmin=meta.minValue, vmax=meta.maxValue)
-                        elif meta.minLoaded :
-                            img_plot = ax.imshow(ascii_data_array, cmap=meta.colormap, extent=asciiHeader.image_extent, interpolation='none', vmax=meta.minValue)
-                        elif meta.maxLoaded :
-                            img_plot = ax.imshow(ascii_data_array, cmap=meta.colormap, extent=asciiHeader.image_extent, interpolation='none', vmax=meta.maxValue)
-                        else :
-                            img_plot = ax.imshow(ascii_data_array, cmap=meta.colormap, extent=asciiHeader.image_extent, interpolation='none')
+                        img_plot = ax.imshow(ascii_data_array, cmap=colorM, extent=asciiHeader.image_extent, interpolation='none')
 
+                # Get the img object in order to pass it to the colorbar function
+                elif meta.cMap :
+                    colorM = ListedColormap(meta.cMap)
+                    if meta.minLoaded and meta.maxLoaded:
+                        img_plot = ax.imshow(ascii_data_array, cmap=colorM, extent=asciiHeader.image_extent, interpolation='none', vmin=meta.minValue, vmax=meta.maxValue)
+                    elif meta.minLoaded :
+                        img_plot = ax.imshow(ascii_data_array, cmap=colorM, extent=asciiHeader.image_extent, interpolation='none', vmax=meta.minValue)
+                    elif meta.maxLoaded :
+                        img_plot = ax.imshow(ascii_data_array, cmap=colorM, extent=asciiHeader.image_extent, interpolation='none', vmax=meta.maxValue)
+                    else :
+                        img_plot = ax.imshow(ascii_data_array, cmap=colorM, extent=asciiHeader.image_extent, interpolation='none')
+                else :
+                    if meta.minLoaded and meta.maxLoaded:
+                        img_plot = ax.imshow(ascii_data_array, cmap=meta.colormap, extent=asciiHeader.image_extent, interpolation='none', vmin=meta.minValue, vmax=meta.maxValue)
+                    elif meta.minLoaded :
+                        img_plot = ax.imshow(ascii_data_array, cmap=meta.colormap, extent=asciiHeader.image_extent, interpolation='none', vmax=meta.minValue)
+                    elif meta.maxLoaded :
+                        img_plot = ax.imshow(ascii_data_array, cmap=meta.colormap, extent=asciiHeader.image_extent, interpolation='none', vmax=meta.maxValue)
+                    else :
+                        img_plot = ax.imshow(ascii_data_array, cmap=meta.colormap, extent=asciiHeader.image_extent, interpolation='none')
+
+                if meta.showbars :
+                    ax_divider = make_axes_locatable(ax)
+                    cax = ax_divider.append_axes("right", size="7%", pad="2%")
                     if meta.ticklist :
                         # Place a colorbar next to the map
-                        cbar = plt.colorbar(img_plot, ticks=meta.ticklist, orientation='vertical', shrink=0.5, aspect=14)
+                        cbar = fig.colorbar(img_plot, ticks=meta.ticklist, orientation='vertical', shrink=0.5, aspect=14, cax=cax)
                     else :
                         # Place a colorbar next to the map
-                        cbar = plt.colorbar(img_plot, orientation='vertical', shrink=0.5, aspect=14)
+                        cbar = fig.colorbar(img_plot, orientation='vertical', shrink=0.5, aspect=14, cax=cax)
                     cbar.set_label(meta.label)
                     if meta.cbarLabel :
                         cbar.ax.set_yticklabels(meta.cbarLabel) 
 
-                    ax.grid(True, alpha=0.5)
+                    if len(subtitles) >= idxRow and len(subtitles[idxRow-1]) > 0 :
+                        ax.set_title(subtitles[idxRow-1])                        
+                ax.set_axis_off()
+                ax.grid(True, alpha=0.5)
+    
+    plt.tight_layout()
+    
     # save image and pdf 
     makeDir(out_path)
     if pdf :
-        pdf.savefig()
+        pdf.savefig(dpi=150)
     plt.savefig(out_path, dpi=150)
     plt.close(fig)
 
