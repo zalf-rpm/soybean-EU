89cce0d95d56ae959f3b327c45902218d5582d7a
sanny <38938782+sannyschulz@users.noreply.github.com>
2021-01-05 15:28:56 +0100

    comment out SID stuff

--- getgrid_window.py
+++ getgrid_window.py
@@ -2,9 +2,9 @@
 # -*- coding: UTF-8
 
 outFile = "./stu_eu_layer_grid.csv"
-sidFile = "./hermes/soil_lookup.csv"
-lat_lon_up = (52.629653, 13.926220)
-lat_lon_down = (52.445819, 14.262035)
+#sidFile = "./hermes/soil_lookup.csv"
+lat_lon_up = (56.327102, 21.181307)
+lat_lon_down = (54.132505, 25.950884)
 
 def findAllSoilRef() :
 
@@ -27,23 +27,25 @@ def findAllSoilRef() :
             if latitude <= lat_lon_up[0] and latitude >= lat_lon_down[0] and longitude >= lat_lon_up[1] and longitude <= lat_lon_down[1] :
                 outSet.add(soil_ref)
 
-    with open(sidFile) as sourcefile:
-        #soil_ref,SID
-        firstLine = True
-        header = dict()
-        for line in sourcefile:
-            if firstLine :
-                firstLine = False
-                header = ReadHeader(line)
-                continue
-            out = loadLine(line)
-            soil_ref = out[header["soil_ref"]]
-            SID = out[header["SID"]]
-            if soil_ref in outSet :
-                print(soil_ref, SID )
-
-#     for item in outSet :
-#             print(item)
+    # with open(sidFile) as sourcefile:
+    #     #soil_ref,SID
+    #     firstLine = True
+    #     header = dict()
+    #     for line in sourcefile:
+    #         if firstLine :
+    #             firstLine = False
+    #             header = ReadHeader(line)
+    #             continue
+    #         out = loadLine(line)
+    #         soil_ref = out[header["soil_ref"]]
+    #         SID = out[header["SID"]]
+    #         if soil_ref in outSet :
+    #             print(soil_ref, SID )
+    
+    outList = sorted(outSet)
+    #print(outList)
+    for item in outList :
+        print(item)
 
 
 def ReadHeader(line) : 
