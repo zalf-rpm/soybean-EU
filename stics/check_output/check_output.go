c31ce4220fb4092aec43008c15d4cdd0f58f2279
sanny <38938782+sannyschulz@users.noreply.github.com>
2021-03-15 16:41:48 +0100

    find missing soilref ids

---
+++ stics/check_output/check_output.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+const soilRefNumber = 99637
+
+func main() {
+
+	sourcePtr := flag.String("source", "D:/stics/out", "path to source folder")
+	sourceFolder := *sourcePtr
+
+	filelist, err := ioutil.ReadDir(sourceFolder)
+	if err != nil {
+		log.Fatal(err)
+	}
+	existingFiles := make(map[int]bool, soilRefNumber)
+	for _, file := range filelist {
+		refIDStr := strings.Split(strings.Split(file.Name(), ".")[0], "_")[3]
+		refID64, err := strconv.ParseInt(refIDStr, 10, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		existingFiles[int(refID64)] = true
+	}
+
+	for i := 1; i <= soilRefNumber; i++ {
+		if _, ok := existingFiles[i]; !ok {
+			fmt.Printf("%d\n", i)
+		}
+	}
+
+}
