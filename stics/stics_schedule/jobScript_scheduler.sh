00ecca7afbd11be65f7148444058ed9a339abf6f
sanny <38938782+sannyschulz@users.noreply.github.com>
2021-06-23 12:19:34 +0200

    sheduler scripts

---
+++ stics/stics_schedule/jobScript_scheduler.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -x
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=80
+#SBATCH --partition=compute
+#SBATCH --job-name=stics_sschulz
+#SBATCH --time=99:00:00
+#SBATCH --output=missLegGapSched_%A.out
+#SBATCH --error=missLegGapSched_%A.err
+
+START=$1
+END=$2
+PARALLEL_JOBS=40
+OUTPUT_FOLDER=$3
+
+INTERNAL_OUTPUT_FOLDER=outputsLGGISS
+SOURCE_PATH=/beegfs/rpm/projects/stics/soybeanEU/scratch
+OUT_PATH=/beegfs/rpm/projects/stics/soybeanEU/outputs
+CLIMATE_PATH=/beegfs/rpm/projects/stics/soybeanEU/climate
+
+DATE=`date +%Y-%d-%B_%H%M%S`
+TEMPWORKFOLDER=/scratch/rpm/stics_${START}_${END}_TEMP_${DATE}
+mkdir -p $TEMPWORKFOLDER
+
+cp -a $SOURCE_PATH/. $TEMPWORKFOLDER
+
+HOMEFOLDER=$TEMPWORKFOLDER
+
+mkdir -p $HOMEFOLDER/$INTERNAL_OUTPUT_FOLDER
+mkdir -p $HOMEFOLDER/log
+
+cd ${HOMEFOLDER} 
+SINGULARITY_HOME=${HOMEFOLDER}
+export SINGULARITY_HOME
+
+./stics_schedule -homePath $HOMEFOLDER -climatePath $CLIMATE_PATH -concurrent $PARALLEL_JOBS -start $START -end $END
+
+mkdir -p $OUT_PATH/$OUTPUT_FOLDER
+#mkdir -p $OUT_PATH/log
+
+#cp -a $HOMEFOLDER/log/. $OUT_PATH/log
+cp -a $HOMEFOLDER/$INTERNAL_OUTPUT_FOLDER/. $OUT_PATH/$OUTPUT_FOLDER
+
+rm -rf $TEMPWORKFOLDER
---
+++ stics/stics_schedule/start_missing.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+
+NODES=30
+SETUPS=8734
+
+DATE=`date +%Y-%d-%B_%H%M%S`
+
+#calculate distribution nodes
+INC=$(($SETUPS / $NODES))
+
+NUM_LEFT=$(($SETUPS % $NODES))
+if [ $NUM_LEFT -ne 0 ] ; then 
+  INC=$(($INC + 1))
+fi
+
+START=0
+END=0
+for (( INDEX=1; INDEX<=$NODES; INDEX++ ))
+do  
+    START=$(($END + 1))
+    END=$(($INC * $INDEX))
+    echo "$START $END missing_${INDEX}_${DATE}"
+    
+    BATCHID=$( sbatch --parsable jobScript_scheduler.sh $START $END missing_${INDEX}_${DATE} )
+    echo "BatchID: $BATCHID"
+done
