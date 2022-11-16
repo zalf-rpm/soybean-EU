f10d38fb79ce13ca4d7fbba98f895cfe26e3b89b
sanny <38938782+sannyschulz@users.noreply.github.com>
2022-11-16 16:35:23 +0100

    fix title

--- combine_outputs/create_image_setup_15_85.sh
+++ combine_outputs/create_image_setup_15_85.sh
@@ -209,8 +209,8 @@ cp ${SOURCEFOLDER}/asciigrid_combined/dev/all_future_stdDev.asc.gz ${TARGETFOLDE
 cat ${SOURCEFOLDER}/asciigrid_combined/dev/all_future_stdDev.asc.meta  ./with_85/yLabel_latidude.meta > ${TARGETFOLDER}/all_future_stdDev85.asc.meta
 
 sed -i 's/title: .*/title: a/g' ${TARGETFOLDER}/all_historical_stdDev.asc.meta
-sed -i 's/title: .*/title: b/g' ${TARGETFOLDER}/all_future_stdDev45.asc.meta
-sed -i 's/title: .*/title: c/g' ${TARGETFOLDER}/all_future_stdDev85.asc.meta
+sed -i 's/title: .*/title: a/g' ${TARGETFOLDER}/all_future_stdDev45.asc.meta
+sed -i 's/title: .*/title: a/g' ${TARGETFOLDER}/all_future_stdDev85.asc.meta
 
 sed -i 's/pink/cyan/g' ${TARGETFOLDER}/all_future_stdDev45.asc.meta
 sed -i 's/pink/cyan/g' ${TARGETFOLDER}/all_future_stdDev85.asc.meta
